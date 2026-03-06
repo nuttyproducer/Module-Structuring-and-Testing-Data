0aa29eefa3fbba355c6cc6900cfdaa5209362de8
Ben Solar <benjamin.vanderzeypen@gmail.com>
2026-03-06 13:37:03 +0100

    Complete Sprint-1/1-key-exercises

--- Sprint-1/1-key-exercises/2-initials.js
+++ Sprint-1/1-key-exercises/2-initials.js
@@ -5,7 +5,8 @@ let lastName = "Johnson";
 // Declare a variable called initials that stores the first character of each string.
 // This should produce the string "CKJ", but you must not write the characters C, K, or J in the code of your solution.
 
-let initials = ``;
+const initials = firstName[0] + middleName[0] + lastName[0]; // Or const initials = `${firstname[0]}${middlename[0]}${lastname[0]}`;
+
+console.log(`The initials are: ${initials}`); // Template literal + Template expression
 
 // https://www.google.com/search?q=get+first+character+of+string+mdn
-
--- Sprint-1/1-key-exercises/4-random.js
+++ Sprint-1/1-key-exercises/4-random.js
@@ -7,3 +7,10 @@ const num = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
 // Try breaking down the expression and using documentation to explain what it means
 // It will help to think about the order in which expressions are evaluated
 // Try logging the value of num and running the program several times to build an idea of what the program is doing
+
+// Math.random() will provide you a number between 0 and 1 for example 0.99 or 0.24
+// Math.floor rounds down your number to the closest whole number. example: 3,63 -> 3
+
+// The code generates a random number between 0 and 100
+
+console.log(num);
