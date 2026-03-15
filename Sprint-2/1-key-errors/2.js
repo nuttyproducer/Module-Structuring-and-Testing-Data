d936a6040ffc216197dddf6340966ad7b6fcf35a
Ben Solar <benjamin.vanderzeypen@gmail.com>
2026-03-15 22:37:04 +0100

    Fix Sprint 2 - key-errors

--- Sprint-2/1-key-errors/2.js
+++ Sprint-2/1-key-errors/2.js
@@ -1,20 +1,25 @@
-
 // Predict and explain first BEFORE you run any code...
 
 // this function should square any number but instead we're going to get an error
 
 // =============> write your prediction of the error here
 
-function square(3) {
-    return num * num;
-}
+// function square(3) {
+//     return num * num;
+// }
 
 // =============> write the error message here
-
+// First of all we won't get an error message because the function is never called.
+// To get an error message we need to add something like console.log to call the function.
 // =============> explain this error message here
-
+// SyntaxError: Unexpected number
 // Finally, correct the code to fix the problem
 
 // =============> write your new code here
+function square(num) {
+  return num * num;
+}
 
+console.log(square(5));
 
+// Outcome: 25
