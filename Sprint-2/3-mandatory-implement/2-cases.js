ab7491b4bc11b1a7ec0a5eb9f329542e44d035a7
Ben Solar <benjamin.vanderzeypen@gmail.com>
2026-03-15 22:37:17 +0100

    Fix Sprint 2 - mandatory-implement

--- Sprint-2/3-mandatory-implement/2-cases.js
+++ Sprint-2/3-mandatory-implement/2-cases.js
@@ -14,3 +14,11 @@
 // You will need to come up with an appropriate name for the function
 // Use the MDN string documentation to help you find a solution
 // This might help https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/toUpperCase
+
+function toUpperSnakeCase(str) {
+  return str.toUpperCase().replaceAll(" ", "_");
+}
+
+console.log(toUpperSnakeCase("lord of the rings"));
+
+// Output: LORD_OF_THE_RINGS
