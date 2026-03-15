c2e282959daf974ce7667cad598e03a9a0af41c2
Ben Solar <benjamin.vanderzeypen@gmail.com>
2026-03-15 22:37:10 +0100

    Fix Sprint 2 - mandatory-debug

--- Sprint-2/2-mandatory-debug/2.js
+++ Sprint-2/2-mandatory-debug/2.js
@@ -3,22 +3,39 @@
 // Predict the output of the following code:
 // =============> Write your prediction here
 
-const num = 103;
+// We don't need the const num on the start. We can start our function directly, but we need to give it a parameter function getLastDigit(num).
+// Even if you call getLastDigit(42), the function ignores the 42.
+// I think our output in the terminal will always be 3.
 
-function getLastDigit() {
+// const num = 103;
+
+// function getLastDigit() {
+//   return num.toString().slice(-1);
+// }
+
+// console.log(`The last digit of 42 is ${getLastDigit(42)}`);
+// console.log(`The last digit of 105 is ${getLastDigit(105)}`);
+// console.log(`The last digit of 806 is ${getLastDigit(806)}`);
+
+// Now run the code and compare the output to your prediction
+// =============> write the output here
+// The last digit of 42 is 3
+// The last digit of 105 is 3
+// The last digit of 806 is 3
+// =============> write your explanation here
+
+// Exactly as i predicted, because we fixed the variable num with a const. The function uses it and the output will always be 3.
+
+// Finally, correct the code to fix the problem
+// =============> write your new code here
+
+function getLastDigit(num) {
   return num.toString().slice(-1);
 }
 
 console.log(`The last digit of 42 is ${getLastDigit(42)}`);
 console.log(`The last digit of 105 is ${getLastDigit(105)}`);
 console.log(`The last digit of 806 is ${getLastDigit(806)}`);
-
-// Now run the code and compare the output to your prediction
-// =============> write the output here
-// Explain why the output is the way it is
-// =============> write your explanation here
-// Finally, correct the code to fix the problem
-// =============> write your new code here
 
 // This program should tell the user the last digit of each number.
 // Explain why getLastDigit is not working properly - correct the problem
