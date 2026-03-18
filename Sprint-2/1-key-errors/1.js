7838a3fb246944757e6e731d0a167b91764f90e7
Ben Solar <benjamin.vanderzeypen@gmail.com>
2026-03-18 11:09:14 +0100

    Fix bug 1-key-errors/1.js
    I'd forgotten to comment out the first part of the code which was calling the function twice.

--- Sprint-2/1-key-errors/1.js
+++ Sprint-2/1-key-errors/1.js
@@ -5,14 +5,14 @@
 
 // Try playing computer with the example to work out what is going on
 
-function convertToPercentage(decimalNumber) {
-  const decimalNumber = 0.5; // We can delete this declaration and use 0.5 in the console.
-  const percentage = `${decimalNumber * 100}%`;
+// function convertToPercentage(decimalNumber) {
+//   const decimalNumber = 0.5; // We can delete this declaration and use 0.5 in the console.
+//   const percentage = `${decimalNumber * 100}%`;
 
-  return percentage;
-}
+//   return percentage;
+// }
 
-console.log(decimalNumber); // Can't run declaration outside of the function.
+// console.log(decimalNumber); // Can't run declaration outside of the function.
 
 // =============> write your explanation here
 
@@ -25,3 +25,5 @@ function convertToPercentage(decimalNumber) {
 }
 
 console.log(convertToPercentage(0.5));
+
+// I'd forgotten to comment out the first part of the code: Bug fixed.
