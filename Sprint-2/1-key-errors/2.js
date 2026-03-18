f208836759f02eaa8c6f19a4a01261b8deda7c48
Ben Solar <benjamin.vanderzeypen@gmail.com>
2026-03-18 11:53:38 +0100

    Refactor to use mathPow in the function.

--- Sprint-2/1-key-errors/2.js
+++ Sprint-2/1-key-errors/2.js
@@ -16,10 +16,33 @@
 // Finally, correct the code to fix the problem
 
 // =============> write your new code here
+// function square(num) {
+//   return num * num;
+// }
+
 function square(num) {
-  return num * num;
+  return mathPow(num, 2);
 }
 
 console.log(square(5));
 
 // Outcome: 25
+
+// Why use Math.pow?
+// It’s flexible: you can raise to any power, not just 2.
+// It’s built-in and works with decimals, negatives, and roots.
+
+// Examples:
+
+// Square a number (raise to the power of 2):
+// Math.pow(5, 2); // 25
+// Math.pow(3, 2); // 9
+
+// Cube a number (raise to the power of 3):
+// Math.pow(2, 3); // 8
+// Math.pow(4, 3); // 64
+
+// Raise to any power:
+// Math.pow(10, 0);   -> 1 (any number to the power of 0 is 1)
+// Math.pow(2, 4);    -> 16
+// Math.pow(9, 0.5);  -> 3 (square root of 9)
