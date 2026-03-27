744bb00525384c26eddbc4f849060f3efbed0d16
Ben Solar <benjamin.vanderzeypen@gmail.com>
2026-03-27 15:44:00 +0100

    Fix: get-ordinal-number function & jest tests.
    Wow! this was an amazing one where I learned a lot of new things! Great thanks!

--- Sprint-3/2-practice-tdd/get-ordinal-number.js
+++ Sprint-3/2-practice-tdd/get-ordinal-number.js
@@ -1,5 +1,23 @@
-function getOrdinalNumber(num) {
-  return "1st";
+function getOrdinalNumber(inputNumber) {
+  inputNumber = Number(inputNumber); // Convert input to a number (handles string inputs edge case)
+  const absoluteNumber = Math.abs(inputNumber); // Use absolute value (handles negatives)
+  const lastTwo = absoluteNumber % 100; // Get the last two digits (for 'teens' exception)
+  let suffix = "th"; // Default suffix is 'th'
+
+  if (lastTwo < 11 || lastTwo > 13) {
+    // || is the "or" operator i learned by solving this function
+    // If not 11, 12, or 13, use last digit rule
+    const lastDigit = absoluteNumber % 10; // Get the last digit
+
+    if (lastDigit === 1) {
+      suffix = "st"; // 1st, 21st, etc.
+    } else if (lastDigit === 2) {
+      suffix = "nd"; // 2nd, 22nd, etc.
+    } else if (lastDigit === 3) {
+      suffix = "rd"; // 3rd, 23rd, etc.
+    }
+  }
+  return inputNumber + suffix; // Return the number with the correct suffix
 }
 
-module.exports = getOrdinalNumber;
+module.exports = getOrdinalNumber; // Export the function for use in other files
--- Sprint-3/2-practice-tdd/get-ordinal-number.test.js
+++ Sprint-3/2-practice-tdd/get-ordinal-number.test.js
@@ -11,3 +11,59 @@ const getOrdinalNumber = require("./get-ordinal-number");
 test("should return '1st' for 1", () => {
   expect(getOrdinalNumber(1)).toEqual("1st");
 });
+
+// While i was doing my research I stumbled upon .toBe for exact equality.
+// .toBe() checks for exact equality using === (strict equality).
+// You can use it for primitive values: strings, numbers, booleans, null, or undefined.
+
+// Basics
+test("should return '2nd' for 2", () => {
+  expect(getOrdinalNumber(2)).toBe("2nd");
+});
+
+test("should return '3rd' for 3", () => {
+  expect(getOrdinalNumber(3)).toBe("3rd");
+});
+
+test("should return '4th' for 4", () => {
+  expect(getOrdinalNumber(4)).toBe("4th");
+});
+
+// Teens ordinal numbers
+test("should return '11th' for 11", () => {
+  expect(getOrdinalNumber(11)).toBe("11th");
+});
+
+test("should return '12th' for 12", () => {
+  expect(getOrdinalNumber(12)).toBe("12th");
+});
+
+test("should return '13th' for 13", () => {
+  expect(getOrdinalNumber(13)).toBe("13th");
+});
+
+// Last-digit rule
+test("should return '21st' for 21", () => {
+  expect(getOrdinalNumber(21)).toBe("21st");
+});
+
+test("should return '22nd' for 22", () => {
+  expect(getOrdinalNumber(22)).toBe("22nd");
+});
+
+test("should return '23rd' for 23", () => {
+  expect(getOrdinalNumber(23)).toBe("23rd");
+});
+
+test("should return '24th' for 24", () => {
+  expect(getOrdinalNumber(24)).toBe("24th");
+});
+
+// Edge cases
+test("should return '0th' for 0", () => {
+  expect(getOrdinalNumber(0)).toBe("0th");
+});
+
+test("should return '-1st' for -1", () => {
+  expect(getOrdinalNumber(-1)).toBe("-1st");
+});
