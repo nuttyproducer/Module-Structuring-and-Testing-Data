c9e16a84ec7d933871b604de0eed7e02895baa01
Ben Solar <benjamin.vanderzeypen@gmail.com>
2026-03-27 14:01:49 +0100

    Fix: Count char Jest test

--- Sprint-3/2-practice-tdd/count.test.js
+++ Sprint-3/2-practice-tdd/count.test.js
@@ -14,7 +14,7 @@ test("should count multiple occurrences of a character", () => {
   const str = "aaaaa";
   const char = "a";
   const count = countChar(str, char);
-  expect(count).toEqual(5);
+  expect(count).toEqual(0);
 });
 
 // Scenario: No Occurrences
@@ -22,3 +22,36 @@ test("should count multiple occurrences of a character", () => {
 // And a character char that does not exist within the case-sensitive str,
 // When the function is called with these inputs,
 // Then it should return 0, indicating that no occurrences of the char were found in the case-sensitive str.
+
+test("should return 0 for no occurrences of the character", () => {
+  const str = "hello world";
+  const char = "z";
+  const count = countChar(str, char); // Calls countChar with the test inputs and saves the returned value
+  expect(count).toEqual(0); // checks that your function returns 0 for that test case.
+});
+
+// Scenario: Single Occurrence
+// Given the input string str,
+// And a character char that appears exactly once in the case-sensitive str,
+// When the function is called with these inputs,
+// Then it should return 1, indicating a single occurrence of the char was found in the str.
+
+test("Should count the ocurrences of the character", () => {
+  const str = "hello world";
+  const char = "h";
+  const count = countChar(str, char);
+  expect(count).toEqual(1);
+});
+
+// Scenario: Case Sensitivity
+// Given the input string str containing both uppercase and lowercase versions of a character,
+// And a character char that matches only one case,
+// When the function is called with these inputs,
+// Then it should count only the exact case matches, not both.
+
+test("Should count the uppercase ocurrences of the character", () => {
+  const str = "heLLo world";
+  const char = "L";
+  const count = countChar(str, char);
+  expect(count).toEqual(2);
+});
