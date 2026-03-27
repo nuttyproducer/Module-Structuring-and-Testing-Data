98809d461d15d1f98acbb7b0ddb1b65b78578102
Ben Solar <benjamin.vanderzeypen@gmail.com>
2026-03-27 14:44:50 +0100

    Fix: Wrong number in .toEqual()
    I wrote 1 instead of 5 due to missed correction on an other test.

--- Sprint-3/2-practice-tdd/count.test.js
+++ Sprint-3/2-practice-tdd/count.test.js
@@ -14,7 +14,7 @@ test("should count multiple occurrences of a character", () => {
   const str = "aaaaa";
   const char = "a";
   const count = countChar(str, char);
-  expect(count).toEqual(0);
+  expect(count).toEqual(5);
 });
 
 // Scenario: No Occurrences
