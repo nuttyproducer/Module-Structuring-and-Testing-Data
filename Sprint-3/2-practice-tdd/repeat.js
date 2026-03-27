70f9dea5bb35d76898c1447f894b8e444c3f72f6
Ben Solar <benjamin.vanderzeypen@gmail.com>
2026-03-27 14:46:00 +0100

    Fix: Updated test file + update function in repeat.js

--- Sprint-3/2-practice-tdd/repeat.js
+++ Sprint-3/2-practice-tdd/repeat.js
@@ -1,5 +1,19 @@
-function repeat() {
-  return "hellohellohello";
+function repeat(str, count) {
+  if (count < 0) {
+    throw new Error("Count must be non-negative number"); // 'throw' creates and sends an error, stopping the function immediately
+  }
+  if (count === 0) {
+    return "";
+  }
+  if (count === 1) {
+    return str;
+  }
+  let result = ""; // Start with an empty string to build the result
+  for (let i = 0; i < count; i++) {
+    // Loop 'count' times (i starts at 0, goes up to count +1)
+    result += str; // Used an addition assignment operator. instead of result = result + str
+  }
+  return result;
 }
 
 module.exports = repeat;
--- Sprint-3/2-practice-tdd/repeat.test.js
+++ Sprint-3/2-practice-tdd/repeat.test.js
@@ -21,12 +21,32 @@ test("should repeat the string count times", () => {
 // When the repeat function is called with these inputs,
 // Then it should return the original str without repetition, ensuring that a count of 1 results in no repetition.
 
+test("should count a single occurrence of the character", () => {
+  const str = "hello";
+  const count = 1;
+  const repeatedStr = repeat(str, count);
+  expect(repeatedStr).toEqual("hello");
+});
+
 // case: Handle Count of 0:
 // Given a target string str and a count equal to 0,
 // When the repeat function is called with these inputs,
 // Then it should return an empty string, ensuring that a count of 0 results in an empty output.
 
+test("should return an empty string when count is 0", () => {
+  const str = "hello";
+  const count = 0;
+  const repeatedStr = repeat(str, count);
+  expect(repeatedStr).toEqual("");
+});
+
 // case: Negative Count:
 // Given a target string str and a negative integer count,
 // When the repeat function is called with these inputs,
 // Then it should throw an error or return an appropriate error message, as negative counts are not valid.
+
+test("should trow an error when count is negative", () => {
+  const str = "hello";
+  const count = -2;
+  expect(() => repeat(str, count)).toThrow("Count must be non-negative number");
+});
